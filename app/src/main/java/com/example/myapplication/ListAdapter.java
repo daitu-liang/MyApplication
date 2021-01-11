1082698382adfa381444328778f7c9f26510e1fa
leixiaoliang <xiaoliang.lei@usmarthk.com>
2021-01-11 16:48:46 +0800

    feat:列表

--- app/src/main/java/com/example/myapplication/ListAdapter.java
+++ app/src/main/java/com/example/myapplication/ListAdapter.java
@@ -1,4 +1,50 @@
 package com.example.myapplication;
 
-public class ListAdapter {
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.List;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class ListAdapter extends RecyclerView.Adapter<ListAdapter.ItemViewHold> {
+    private  List<String> dataList;
+    private  Context context;
+
+    ListAdapter(Context context, List<String> dataList){
+        this.context=context;
+        this.dataList=dataList;
+    }
+
+    @NonNull
+    @Override
+    public ItemViewHold onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+        View itemView= LayoutInflater.from(context).inflate(R.layout.item_layout,parent);
+        return new ItemViewHold(itemView);
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ItemViewHold holder, int position) {
+
+        holder.tv.setText("-->"+dataList.get(position));
+    }
+
+    class  ItemViewHold extends RecyclerView.ViewHolder{
+        private TextView tv;
+
+        public ItemViewHold(@NonNull View itemView) {
+            super(itemView);
+            tv=itemView.findViewById(R.id.tv);
+        }
+    }
 }
--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,11 +1,21 @@
 package com.example.myapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView rv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,16 +23,48 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         testGit();
 
+        ((Button)findViewById(R.id.btn1)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        ((Button)findViewById(R.id.btn2)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 
     private void testGit() {
 
-        RecyclerView rv = (RecyclerView) findViewById(R.id.rv);
+         rv = (RecyclerView) findViewById(R.id.rv);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
+
+        rv.setLayoutManager(linearLayoutManager);
+
+        ListAdapter listAdapter = new ListAdapter(this, getList());
+        rv.setAdapter(listAdapter);
         ///1111111111
         ///aaaa
         //
         //
         //
+        rv.addOnScrollListener(new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrollStateChanged(@NonNull RecyclerView recyclerView, int newState) {
+                super.onScrollStateChanged(recyclerView, newState);
+            }
+        });
 
+    }
+
+    private List<String> getList() {
+        List<String> mlist=new ArrayList<>();
+        for (int i = 0; i <30; i++) {
+            mlist.add("item-"+i);
+        }
+        return mlist;
     }
 }
