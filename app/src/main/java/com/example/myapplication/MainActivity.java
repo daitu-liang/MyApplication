f793d04d80621e6c6a0a241eba3adecda6508744
leixiaoliang <xiaoliang.lei@usmarthk.com>
2021-01-12 16:36:35 +0800

    fix:ccc

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -55,7 +55,7 @@ public class MainActivity extends AppCompatActivity {
                 if(newState==RecyclerView.SCROLL_STATE_IDLE){
                     //aaaa
                     ///bbb
-                    ///
+                    ///ccc
 
 
 
