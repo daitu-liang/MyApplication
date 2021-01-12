c9b6f9e76ed16279b7d286f565630217a0acc92e
leixiaoliang <xiaoliang.lei@usmarthk.com>
2021-01-12 09:52:18 +0800

    opt:dddd--modify-2

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -70,6 +70,7 @@ public class MainActivity extends AppCompatActivity {
 
                     //bbbbbbbbbbbbbb
                     //cccccccccccc
+                    //dddd
                 }
             }
         });
