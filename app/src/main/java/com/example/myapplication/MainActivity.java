55d5b04e9bbb4afc5fc1e1f78b658439cc4a2f1f
leixiaoliang <xiaoliang.lei@usmarthk.com>
2021-01-14 14:05:59 +0800

    feat: 1111

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -29,6 +29,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         testGit();
 
+        ///111111111111
 
 
         llContainer=(LinearLayout)findViewById(R.id.ll_container);
