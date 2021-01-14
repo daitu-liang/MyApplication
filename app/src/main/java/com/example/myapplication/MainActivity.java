0e2780e847bf90314f382b23bd598a49638d5a1a
leixiaoliang <xiaoliang.lei@usmarthk.com>
2021-01-14 11:15:35 +0800

    feat: modfied (Amend commit,force push)

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -37,8 +37,12 @@ public class MainActivity extends AppCompatActivity {
 
         tvdes=(TextView)findViewById(R.id.tv_des);
         tvdes.setText("5555咔咔咔咔咔咔扩扩扩扩扩扩扩扩扩扩扩555555");
-        tvdes.setText("44444444444qwwqwq:");
+        tvdes.setText("44444444444qwwqwq:www");
+        ////////////
 
+        //dddddddd
+///dddoooooooooooooo
+       // cc
         ((Button) findViewById(R.id.btn1)).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
